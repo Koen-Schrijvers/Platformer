8330f2093f07344d6d416035b8a2f7938e1fea5f
Koen-Schrijvers <koen.schrijvers@student.ap.be>
2023-04-05 23:44:08 +0200

    added some basic movement

--- Platformer/Entities/PlayerCharacter.cs
+++ Platformer/Entities/PlayerCharacter.cs
@@ -6,26 +6,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Platformer.AnimationUtil;
+using SharpDX.MediaFoundation;
+using Platformer.Input;
 
 namespace Platformer.Entities
 {
     internal class PlayerCharacter
     {
+        //animation
         private Texture2D texture;
         private List<Animation> animations;
+
+        //input
+        private KeyboardDirectionTranslator keyboardDirectionTranslator;
+
+        //movement
+      
+        private Vector2 currentDirection;
+        private Vector2 position;
+        private Vector2 baseSpeed;
+        private Vector2 currentSpeed;
+     
+        private float gravity;
+        private float jumpImpulse;
         public PlayerCharacter(Texture2D texture)
         {
             this.texture = texture;
             this.animations = SpriteCutter.CreateAnimations(texture, new int[7] {11, 12, 1, 6, 1, 5, 7});
+            this.position = new Vector2(300,300);
+            this.currentDirection = new Vector2(0, 0);
+            this.baseSpeed = new Vector2(3, 0);
+            this.currentSpeed= new Vector2(0, 0);
+            this.jumpImpulse = 10f;
+            this.gravity = 1f;
+            this.keyboardDirectionTranslator = new KeyboardDirectionTranslator();
         }
 
         public void Update(GameTime gameTime)
         {
-            this.animations[1].Update(gameTime);
+            Move();
+            Animate(gameTime);
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, new Vector2(100, 100), this.animations[1].CurrentFrame, Color.White);
+            spriteBatch.Draw(texture, position, animations[1].CurrentFrame, Color.White);
+        }
+
+        public void Move()
+        {
+            currentDirection = keyboardDirectionTranslator.TranslateInputToDirection();
+            currentSpeed.X = baseSpeed.X * currentDirection.X;
+            if (currentDirection.Y == -1) {
+                //jump
+                currentSpeed.Y -= jumpImpulse;
+            }
+            currentSpeed.Y += gravity;
+            position += currentSpeed;
+            if(position.Y > 300)
+            {
+                position.Y = 300;
+                currentSpeed.Y = 0;
+            }
+        }
+        public void Animate(GameTime gameTime)
+        {
+            this.animations[1].Update(gameTime);
         }
     }
 }
---
+++ Platformer/Input/KeyboardDirectionTranslator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer.Input
+{
+    internal class KeyboardDirectionTranslator
+    {
+        private bool spacebar = false;
+        public Vector2 TranslateInputToDirection()
+        {
+            KeyboardState keyboard = Keyboard.GetState();
+            Vector2 direction = new Vector2(0,1); // is altijd 1 wegens zwaartekracht
+            if (keyboard.IsKeyDown(Keys.D))
+            {
+                direction.X = 1;
+            }
+            else if(keyboard.IsKeyDown(Keys.Q))
+            {
+                direction.X = -1;
+            }
+
+
+            if (keyboard.IsKeyDown(Keys.Space) && !spacebar)
+            { 
+                direction.Y = -1; 
+                spacebar = true;
+            }
+            if(keyboard.IsKeyUp(Keys.Space)) 
+            {
+                spacebar =  false;
+            }
+            return direction;
+        }
+    }
+}
