1733f483f053166e2ca752bbabb76d283bef99b4
Koen-Schrijvers <koen.schrijvers@student.ap.be>
2023-08-21 16:52:29 +0200

    Apply small changes to CollisionEvents

--- Platformer/Utilities/CollisionEvents/BasicEnemyCollisionEvent.cs
+++ Platformer/Utilities/CollisionEvents/BasicEnemyCollisionEvent.cs
@@ -7,28 +7,24 @@ namespace Platformer.Utilities.CollisionEvents
 {
     internal class BasicEnemyCollisionEvent : BaseCollisionEvent
     {
-        private BaseEnemy subject;
-        public BasicEnemyCollisionEvent(BaseEnemy subject)
-        {
-            this.subject = subject;
-        }
         public override void Execute(ICollidable other, ICollidable thisObject)
         {
-            float overlapX = CalculateOverlapX(other.Hitbox, subject.Hitbox);
-            float overlapY = CalculateOverlapY(other.Hitbox, subject.Hitbox);
             PlayerCharacter p = other as PlayerCharacter;
-            if (p == null) return;
+            BaseEnemy e = thisObject as BaseEnemy;
+            if (p == null || e == null) return;
+            float overlapX = CalculateOverlapX(other.Hitbox, e.Hitbox);
+            float overlapY = CalculateOverlapY(other.Hitbox, e.Hitbox);
             if (Math.Abs(overlapX /= p.Hitbox.Width) >= Math.Abs(overlapY /= p.Hitbox.Height) && overlapY > 0)
             {
                 p.CurrentSpeedY = -4f;
                 p.IsGrounded = true;
-                subject.TakeDamage(1);
+                e.TakeDamage(1);
             }
             else
             {
                 p.TakeDamage(1);
                 p.DisableInput(0.3d);
-                subject.CurrentDirection *= -1;
+                e.CurrentDirection *= -1;
                 if (overlapX < 0)
                 {
                     p.KnockBack(5f, -2f);
